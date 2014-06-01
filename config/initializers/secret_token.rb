7ac6a3b7b840407171243d9802ca39f532cc56b5
Tinashe Mashungu <mashungut@gmail.com>
2014-06-01 17:58:31 +0000

    Creaate rails scaffold for sample_app with the command
    rbenv exec rails new . --skip-test-unit
    
    Suppress 'test' directory in Rails in order to use RSpec instead

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '320efea946bedfc76837ca7bdd02fd7ee37d090c9438f5e7ad7f95d99f15a92c73ce21dab20c7f764576167a138f3a3631156cd65656e6365a48d0e7eb40fbd9'
