101c7cd8026caac7cb8eef4f09683456187cc676
Tinashe Mashungu <mashungut@gmail.com>
2014-06-08 19:16:04 +0000

    Reconfigure secret token so it is dynamically generated
    Without this patch the secret token is stored as a string in the repo
    The patch addressed the problem by reading the secret token in a file
    rather than store it in the repo

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '320efea946bedfc76837ca7bdd02fd7ee37d090c9438f5e7ad7f95d99f15a92c73ce21dab20c7f764576167a138f3a3631156cd65656e6365a48d0e7eb40fbd9'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
